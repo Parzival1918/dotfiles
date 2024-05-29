e9ea441853d0a7973cfa0a49bbd990aa7add8997
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2024-05-29 23:19:02 +0100

    add nvim config

--- files/fish/config.fish
+++ files/fish/config.fish
@@ -4,7 +4,7 @@
 {%@@ if devicename == "macos" @@%}
 set PATH $PATH /Users/parzival1918/.local/bin /Users/parzival1918/.detaspace/bin /opt/homebrew/bin /opt/homebrew/sbin /Users/parzival1918/.reflex/.nvm/versions/node/v18.17.0/bin /Users/parzival1918/.bun/bin /System/Cryptexes/App/usr/bin /Users/parzival1918/bin /Library/TeX/texbin /Library/Apple/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin /Users/parzival1918/.cargo/bin /Users/parzival1918/.local/bin /Users/parzival1918/Library/Python/3.11/bin /Users/parzival1918/.local/bin
 {%@@ elif devicename == "ubuntu" @@%}
-set PATH $PATH /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin /home/unstraycato/programs/ovito-basic-3.9.4-x86_64/bin /home/unstraycato/programs/julia-1.9.4/bin /home/unstraycato/programs/travis-src-220729/exe /home/unstraycato/.config/emacs/bin /home/unstraycato/.cargo/bin /home/unstraycato/.local/bin /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /snap/bin /snap/bin 
+set PATH $PATH /home/linuxbrew/.linuxbrew/bin /home/linuxbrew/.linuxbrew/sbin /home/unstraycato/programs/ovito-basic-3.9.4-x86_64/bin /home/unstraycato/programs/julia-1.9.4/bin /home/unstraycato/programs/travis-src-220729/exe /home/unstraycato/.config/emacs/bin /home/unstraycato/.cargo/bin /home/unstraycato/.local/bin /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /snap/bin /snap/bin /home/unstraycato/.local/share/bob/nvim-bin
 {%@@ endif @@%}
 
 # set the prompt, using the preset plain-text-symbols 
